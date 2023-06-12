3fac0380eb56d023c396406248738a862fc1cab6
emmanuel <emmailegbo@yahoo.com>
2023-06-12 19:30:50 +0100

    Portfolio website
    This is my portfolio website coded using the MERN-STACK

---
+++ frontend/src/component/portfolioInfo.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/esm/Container'
+import Row from 'react-bootstrap/esm/Row'
+import Col from 'react-bootstrap/esm/Col'
+import PortfolioData from '../component/portfolioData'
+import Listings from '../component/listings'
+import axios from 'axios'
+
+
+
+
+
+const PortfolioInfo = ()=> {
+  const [data, setData] = useState([])
+
+
+
+  let url = "http://localhost:5000/products";
+
+   
+    const Fetchdata = async ()=> {
+
+          try {
+            axios
+            .get(url)
+            .then((res)=> {
+             
+                setData(res.data);
+                
+            })
+          } catch (error) {
+            console.log(error)
+        
+          }
+       
+                  // try {
+                    
+                  //   let response = await fetch(url);
+                     
+                  //   let actualdata = await response.json();
+                  //       setData(actualdata)
+                  // } catch (error) {
+                  //      console.log(error)
+
+
+                    // }  
+
+  }
+  useEffect(()=>{
+    Fetchdata()
+  }, []);
+
+      
+    return(
+         
+         <div className="port">
+
+            <Container>
+            <Row className =  ' bg- d-flex ' style={{justifyContent: 'space-around'}}>
+            <Col md= {10} className='map m-auto' style={{justifyContent: 'space-around'}}>
+               {data.map((x)=>{
+
+                  return(
+                    
+                  <Listings key={x._id}  {...x} />
+
+                  )
+
+               })}
+            </Col>
+
+
+
+           
+            </Row>   
+            </Container>
+           
+         </div>
+     )
+  
+}
+ export default PortfolioInfo
