f337b912e07d71bd0368a4ddc0b5bb90c0ea0c1d
Emmanuel Ilegbodu <emmaluga101@gmail.com>
2023-11-29 17:10:57 +0000

    added netlify.toml file to root folder

--- frontend/src/component/portfolioInfo.js
+++ frontend/src/component/portfolioInfo.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/esm/Container'
 import Row from 'react-bootstrap/esm/Row'
 import Col from 'react-bootstrap/esm/Col'
 import Listings from '../component/listings'
-import axios from 'axios'
 import portfolioData from "./portfolioData"
 
 
